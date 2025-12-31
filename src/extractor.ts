9d9c6be1cc94c05796e47633f38ba0e160097544
pokemon-card <136928586+pokemon-card@users.noreply.github.com>
2025-12-31 17:17:46 -0500

    Update extractor.ts update url and hopefully make loading faster

--- src/extractor.ts
+++ src/extractor.ts
@@ -8,15 +8,12 @@ import * as cheerio from "cheerio";
 import { fetchAndParseHLS, ParsedHLSStream } from "./hls-utils";
 import { SOURCE_URL } from "./constants";
 
-let BASEDOM = "https://cloudnestra.com";
+let BASEDOM = "https://vidsrc-embed.ru/embed";
 
 // Array of realistic user agents to rotate through
 const USER_AGENTS = [
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0",
-  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15",
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
