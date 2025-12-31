8c0e97ebc0f2ea76f77348ff62de5603238bdd23
pokemon-card <136928586+pokemon-card@users.noreply.github.com>
2025-12-31 17:25:05 -0500

    revert url and update the user agent values (maybe helps with performance idk)

--- src/extractor.ts
+++ src/extractor.ts
@@ -8,15 +8,15 @@ import * as cheerio from "cheerio";
 import { fetchAndParseHLS, ParsedHLSStream } from "./hls-utils";
 import { SOURCE_URL } from "./constants";
 
-let BASEDOM = "https://vidsrc-embed.ru/embed";
+let BASEDOM = "https://cloudnestra.com";
 
 // Array of realistic user agents to rotate through
 const USER_AGENTS = [
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0",
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/140.0",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/128.0.0.0",
   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/142.0",
 ];
 
 // Function to get sec-ch-ua based on user agent
